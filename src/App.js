b2bcdfdf846b4bea14c5e28035ead86046a15c60
Stacy Burris <stacy1burris@gmail.com>
2021-01-12 20:30:13 -0800

    starwars api displaying

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import Header from './header/header';
 import Form from './form/form';
+import Results from './results/results';
 import Footer from './footer/footer';
 import './app.scss';  //base design for site
 
+// Lab requirements
+// app.js - Container
+// Holds state: Count and Results Array
+// A class method that can update state
+// Renders 2 Child Components
 
+// form and results
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: '',
+      results: [],
+      headers: ''
+    };
+  }
+
+  updateResults = (apiResults) => {
+    this.setState({ headers: apiResults.headers });
+    this.setState({ results: apiResults.results });
+    this.setState({ count: apiResults.count });
+  }
 
   render() {
     return (
       <>
-        <Header />
-        <Form />
+        <Header
+          {...this.state.headers}
+        />
+        <Form
+          apiResults={this.updateResults}
+        //results={this.state.results}
+        />
+        <Results
+          count={this.state.count}
+          results={this.state.results}
+
+
+        />
+
         <Footer />
       </>
     )
@@ -21,3 +54,4 @@ class App extends React.Component {
 
 
 export default App;
+
